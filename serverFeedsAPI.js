e46392e087d546b3c622a831aba878c27263d15e
Matthieu <matmon@gmail.com>
2016-11-27 01:12:10 +0100

    Update API

--- serverFeedsAPI.js
+++ serverFeedsAPI.js
@@ -3,6 +3,12 @@ var sqlite = require("sqlite3");
 var db = new sqlite.Database('feeds.sqlite');
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/allCategoriesList', function (req, res) {
 	console.log(new Date(), "allCategoriesList GET ", req.query);
 	db.all("SELECT \
