a524bd4a3586d8e05478697b45290c29c4a67860
Matthieu <matmon@gmail.com>
2017-04-02 19:47:58 +0200

    Using correct port for Socket
    Finding of the correct port to use for the socket app

--- socketIo.js
+++ socketIo.js
@@ -1,10 +1,17 @@
 var server = require('http').createServer();
-var io = require('socket.io')(server);
-io.on('connection', function(client){
-    client.on('event', function(data){});
-    client.on('disconnect', function(){});
-    client.on('updateFeed', function(data){
-        client.broadcast.emit('updateFeed', data);
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync('server.config.json', 'utf8'));
+
+// let's get the port to use for the Socket server
+var re = /.*:(\d+)/g.exec(config.socketHost);
+if(re.length > 0){
+    var io = require('socket.io')(server);
+    io.on('connection', function(client){
+        client.on('event', function(data){});
+        client.on('disconnect', function(){});
+        client.on('updateFeed', function(data){
+            client.broadcast.emit('updateFeed', data);
+        });
     });
-});
-server.listen(3000);
+    server.listen(re[1]);
+}
