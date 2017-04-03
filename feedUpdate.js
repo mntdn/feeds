8dba676d37c72d78531056697657db1ac63ef0ce
Matthieu Montaudouin <m.montaudouin@intra-expaceo.com>
2017-04-03 23:58:53 +0200

    Bug correction to avoid node update not exiting

--- feedUpdate.js
+++ feedUpdate.js
@@ -106,69 +106,88 @@ if(!logTableInit && !mainTableInit){
 			request(feed.Url)
 			    .on('error', function (error) {
 					updateLog(feed.IdFeed, 'Error', error.toString());
+					nbFeedsToUpdate--;
+					if(nbFeedsToUpdate === 0){
+						socket.disconnect();
+					}
 			    })
 			    .on('response', function (res) {
 			        var streamResponse = this;
 			        if (res.statusCode != 200){
+						nbFeedsToUpdate--;
+						if(nbFeedsToUpdate === 0){
+							socket.disconnect();
+						}
 						this.emit('error', new Error(res.statusCode));
-			        }
-			        var feedparser = new FeedParser();
-			        streamResponse.pipe(feedparser);
-			        feedparser
-			            .on('error', function(error) {
-							updateLog(feed.IdFeed, 'Error', error.toString());
-			            })
-			            .on('readable', function() {
-			                var stream = this, meta = this.meta, item;
+			        } else {
+						var feedparser = new FeedParser();
+						streamResponse.pipe(feedparser);
+						feedparser
+							.on('error', function(error) {
+								nbFeedsToUpdate--;
+								if(nbFeedsToUpdate === 0){
+									socket.disconnect();
+								}
+								updateLog(feed.IdFeed, 'Error', error.toString());
+							})
+							.on('readable', function() {
+								var stream = this, meta = this.meta, item;
 
-							db.all("SELECT Hash FROM FeedContent WHERE IdFeed = " + feed.IdFeed, function(e2, content){
-								var nbNewArticles = 0;
-								if(e2 && isVerbose) console.log("Erreur SQL -- ", e2);
-								if(	content.length == 0) {
-									// if there is no article in the DB we download everything
-									if(isVerbose) console.log(feed.IdFeed, feed.Name, " -- First download of news");
-									var stmt = db.prepare("INSERT INTO FeedContent(IdFeed, PublishedDate, Title, Content, Url, Author, Hash) VALUES (?,?,?,?,?,?,?)");
-									var logStatement = dbLog.prepare("INSERT INTO UpdateActions VALUES (?,?,?,?)");
-									while (item = stream.read()) {
-										if(isVerbose) console.log("\t" + item.date + " -- " + item.title);
-										var hash = crypto.createHash('sha256');
-										hash.update(item.title + item.link);
-										var calcHash = hash.digest('hex');
-										stmt.run(feed.IdFeed, item.date, item.title, item.description, item.link, item.author, calcHash);
-										logStatement.run(feed.IdFeed, 'insert', item.date + " -- " + item.title, (new Date()).toISOString());
-										nbNewArticles++;
-					                }
-									stmt.finalize();
-									logStatement.finalize();
-								} else {
-									// first do an array of hashes
-									var hashArray = [];
-									for(var j = 0; j < content.length; j++){
-										hashArray.push(content[j].Hash);
-									}
-									// let's compare hashes found in the DB with the ones we calculate
-									if(isVerbose) console.log(feed.IdFeed, feed.Name, " -- Not up to date");
-									var stmt = db.prepare("INSERT INTO FeedContent(IdFeed, PublishedDate, Title, Content, Url, Author, Hash) VALUES (?,?,?,?,?,?,?)");
-									var logStatement = dbLog.prepare("INSERT INTO UpdateActions VALUES (?,?,?,?)");
-									while (item = stream.read()) {
-										var hash = crypto.createHash('sha256');
-										hash.update(item.title + item.link);
-										var calcHash = hash.digest('hex');
-										if(!arrayFind(hashArray, calcHash)){
-											// if article not found, we add it
+								db.all("SELECT Hash FROM FeedContent WHERE IdFeed = " + feed.IdFeed, function(e2, content){
+									var nbNewArticles = 0;
+									if(e2 && isVerbose) console.log("Erreur SQL -- ", e2);
+									if(	content.length == 0) {
+										// if there is no article in the DB we download everything
+										if(isVerbose) console.log(feed.IdFeed, feed.Name, " -- First download of news");
+										var stmt = db.prepare("INSERT INTO FeedContent(IdFeed, PublishedDate, Title, Content, Url, Author, Hash) VALUES (?,?,?,?,?,?,?)");
+										var logStatement = dbLog.prepare("INSERT INTO UpdateActions VALUES (?,?,?,?)");
+										while (item = stream.read()) {
+											if(isVerbose) console.log("\t" + item.date + " -- " + item.title);
+											var hash = crypto.createHash('sha256');
+											hash.update(item.title + item.link);
+											var calcHash = hash.digest('hex');
 											stmt.run(feed.IdFeed, item.date, item.title, item.description, item.link, item.author, calcHash);
 											logStatement.run(feed.IdFeed, 'insert', item.date + " -- " + item.title, (new Date()).toISOString());
 											nbNewArticles++;
 										}
+										stmt.finalize();
+										logStatement.finalize();
+									} else {
+										// first do an array of hashes
+										var hashArray = [];
+										for(var j = 0; j < content.length; j++){
+											hashArray.push(content[j].Hash);
+										}
+										// let's compare hashes found in the DB with the ones we calculate
+										if(isVerbose) console.log(feed.IdFeed, feed.Name, " -- Not up to date");
+										var stmt = db.prepare("INSERT INTO FeedContent(IdFeed, PublishedDate, Title, Content, Url, Author, Hash) VALUES (?,?,?,?,?,?,?)");
+										var logStatement = dbLog.prepare("INSERT INTO UpdateActions VALUES (?,?,?,?)");
+										while (item = stream.read()) {
+											var hash = crypto.createHash('sha256');
+											hash.update(item.title + item.link);
+											var calcHash = hash.digest('hex');
+											if(!arrayFind(hashArray, calcHash)){
+												// if article not found, we add it
+												stmt.run(feed.IdFeed, item.date, item.title, item.description, item.link, item.author, calcHash);
+												logStatement.run(feed.IdFeed, 'insert', item.date + " -- " + item.title, (new Date()).toISOString());
+												nbNewArticles++;
+											}
+										}
+										stmt.finalize();
+										logStatement.finalize();
 									}
-									stmt.finalize();
-									logStatement.finalize();
-								}
-								socket.emit('updateFeed', {'IdFeed': feed.IdFeed, 'NbNewArticles': nbNewArticles});
-							});
+									nbFeedsToUpdate--;
+									if(nbFeedsToUpdate === 0){
+										socket.disconnect();
+									}
+									socket.emit('updateFeed', {'IdFeed': feed.IdFeed, 'NbNewArticles': nbNewArticles});
+								});
 
-			            });
+							});
+					}
 			    });
 		});
 	});
 }
+
+// socket.disconnect();
